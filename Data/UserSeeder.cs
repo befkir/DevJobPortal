48428f3d5040a9413f6d9102e34168a166260bcb
feshfaa <feshfaa@gmail.com>
2025-03-20 20:17:41 +0300

    Refactored user seeder class

--- Data/UserSeeder.cs
+++ Data/UserSeeder.cs
@@ -1,6 +1,44 @@
-﻿namespace DevJobPortal.Data
+﻿using DevJobPortal.Constants;
+using Microsoft.AspNetCore.Identity;
+
+namespace DevJobPortal.Data
 {
     public class UserSeeder
     {
+        public static async Task SeedUsersAsync(IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            await CreateUserWithRole(userManager, "admin@devjobportal.com", "Admin123!", Roles.Admin);
+            await CreateUserWithRole(userManager, "jobseeker@devjobportal.com", "JobSeeker123!", Roles.JobSeeker);
+            await CreateUserWithRole(userManager, "employer@devjobportal.com", "Employer123!", Roles.Employer);
+        }
+
+        private static async Task CreateUserWithRole(
+            UserManager<IdentityUser> userManager,
+            string email,
+            string password,
+            string role)
+        {
+            if (await userManager.FindByEmailAsync(email) == null)
+            {
+                var user = new IdentityUser
+                {
+                    Email = email,
+                    EmailConfirmed = true,
+                    UserName = email
+                };
+                var result = await userManager.CreateAsync(user, password);
+
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, Roles.Admin);
+                }
+                else
+                {
+                    throw new Exception($"Failed creating user with email {user.Email}. Errors:{string.Join(",", result.Errors)}");
+                }
+
+            }
+        }
     }
 }
